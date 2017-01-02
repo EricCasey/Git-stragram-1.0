888eb6636dd9a9a3829a68192651a02f87a58f67
EricCasey <EricCasey509@gmail.com>
2017-01-02 12:42:01 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import SelectedFoods from './SelectedFoods';
-import FoodSearch from './FoodSearch';
 
 const App = React.createClass({
   getInitialState: function () {
-    return {
-      selectedFoods: [],
-    };
+    return { };
   },
+
   render: function () {
     return (
       <div className='App'>
-        <div className='ui text container'>
-          <SelectedFoods
-            foods={this.state.selectedFoods}
-            onFoodClick={
-              (idx) => (
-                this.setState({
-                  selectedFoods: [
-                    ...this.state.selectedFoods.slice(0, idx),
-                    ...this.state.selectedFoods.slice(
-                      idx + 1, this.state.selectedFoods.length
-                    ),
-                  ],
-                })
-              )
-            }
-          />
-          <FoodSearch
-            onFoodClick={
-              (food) => (
-                this.setState({
-                  selectedFoods: this.state.selectedFoods.concat(food),
-                })
-              )
-            }
-          />
-        </div>
+        <p> testeroo </p>
       </div>
     );
   },
